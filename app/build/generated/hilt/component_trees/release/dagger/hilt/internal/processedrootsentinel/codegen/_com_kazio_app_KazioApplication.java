fff49fd26e5a9210652e7593884108647710ce63
oguzyl11 <oguzhanym271@gmail.com>
2026-07-17 03:26:50 +0300

    fix: remove tracked build folders and add .gitignore

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_kazio_app_KazioApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.kazio.app.KazioApplication"
-)
-public final class _com_kazio_app_KazioApplication {
-}
